cd1c706b440f27c06267bc35f1ec6ff2570eeb3e
Andrew Morgan <163063003+ahmorgan@users.noreply.github.com>
2026-04-29 21:50:43 -0400

    add final inference code

--- vlm_inference.py
+++ vlm_inference.py
@@ -10,29 +10,41 @@ os.environ["PYTORCH_MPS_LOW_WATERMARK_RATIO"] = "0.60"
 from transformers import AutoProcessor, Qwen3VLForConditionalGeneration
 from transformers.video_utils import VideoMetadata
 from tennis_dataset import TennisPointDataset
+import cv2
 
 ANNOTATION_DIR = "data/annotations"
 VIDEO_DIR = "data/videos"
 
-use_frame_proportion = 0.20  # 25 fps / 5 = 5 fps (this is what works on my M4)
+use_frame_proportion = 0.02 # 25 fps / 5 = 5 fps (this is what works on my M4)
+use_checkpoint = True
 
 points_dataset = TennisPointDataset(annotation_dir=ANNOTATION_DIR, video_dir=VIDEO_DIR, use_frame_proportion=use_frame_proportion)  # 25 FPS video
 
-point = points_dataset[0]
+point = points_dataset[43]
 
-frames = point["frames"]
+frames = point["messages"][0]["content"][0]["video"]
 
 if not os.path.isdir("frame_debug"):
     os.mkdir("frame_debug")
 for i, frame in enumerate(frames):
     frame.save(f"frame_debug/frame{i}.png")
 
-device = torch.device("mps") if torch.mps.is_available() else torch.device("cpu")  # change to cuda if using cuda enabled gpu
-model = Qwen3VLForConditionalGeneration.from_pretrained("Qwen/Qwen3-VL-2B-Instruct", dtype="auto", device_map=device).to(device)
+frames_video = [np.array(frame) for frame in frames]
+writer = cv2.VideoWriter('frame_debug/point.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 25 * use_frame_proportion, (frames_video[0].shape[1], frames_video[0].shape[0]))
+for frame in frames_video:
+    writer.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+writer.release()
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")  # change to cuda if using cuda enabled gpu
+
+if use_checkpoint:
+    model = Qwen3VLForConditionalGeneration.from_pretrained("checkpoint/checkpoint-279", dtype="auto", device_map=device).to(device)
+else:
+    model = Qwen3VLForConditionalGeneration.from_pretrained("Qwen/Qwen3-VL-2B-Instruct", dtype="auto", device_map=device).to(device)
 
 # max_pixels controls the visual token size (size of image patches passed to vision encoder transformer)
 # we want *up to* 256 28x28 image patches here (could be smaller because the processor has to snap the image resolution to the nearest dimensions divisible by 28).
-processor = AutoProcessor.from_pretrained("Qwen/Qwen3-VL-2B-Instruct", max_pixels=512 * 28 * 28)
+processor = AutoProcessor.from_pretrained("Qwen/Qwen3-VL-2B-Instruct", max_pixels=512 * 32 * 32)
 
 video_metadata = VideoMetadata(
     total_num_frames=len(frames),
@@ -40,22 +52,8 @@ video_metadata = VideoMetadata(
     duration=len(frames) / (25.0 * use_frame_proportion)
 )
 
-player1, player2 = point["player1"], point["player2"]
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "video",
-                "video": [frame for frame in frames],
-            },
-            {"type": "text", "text": f"This tennis clip shows a point between {player1} and {player2}. The score is {point['score']}. Describe what happens in the tennis clip, focusing on the players' actions and point outcome."}  # WE SHOULD ALSO PUT POINT METADATA HERE
-        ],
-    }
-]
-
 inputs = processor.apply_chat_template(
-    messages,
+    [point["messages"][0]],
     tokenize=True,
     add_generation_prompt=True,
     return_dict=True,
@@ -73,7 +71,7 @@ with torch.no_grad():
     )
 
     print("\nGround truth description:")
-    print(point["desc"])
+    print(point["messages"][1]["content"])
     print("\nModel output:")
     print(output_text)
 
